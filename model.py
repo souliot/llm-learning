4afc3a64e8c05078719d451d24ce65e142404a70
Souliot <linleizhou19910924@163.com>
2024-07-21 16:33:34 +0800

    feat(sensevoice): add sense voice

---
+++ model.py
@@ -0,0 +1,3 @@
+from modelscope.hub.snapshot_download import snapshot_download
+
+model_dir = snapshot_download('Qwen/Qwen-1_8B-Chat', cache_dir='./modelscope')
---
+++ scripts/env_init.sh
@@ -0,0 +1,2 @@
+conda install pytorch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 pytorch-cuda=11.8 -c pytorch -c nvidia -y
+pip install modelscope funasr
